0758266d9a1fb6bacf4689ac4867d8618da23401
Mohannad <mohannad.alumari@gmail.com>
2022-10-18 15:38:12 +0300

    added products page and changed home page

---
+++ dotnet-project/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet_project.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
