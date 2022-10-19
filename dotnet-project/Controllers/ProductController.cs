30eec3f4573395e8ad5bd89838b57d36295fea67
Mohannad <mohannad.alumari@gmail.com>
2022-10-19 12:49:05 +0300

    added Dapper

--- dotnet-project/Controllers/ProductController.cs
+++ dotnet-project/Controllers/ProductController.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using dotnet_project.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 
 namespace dotnet_project.Controllers
@@ -12,16 +16,40 @@ namespace dotnet_project.Controllers
     public class ProductController : Controller
     {
         private readonly ILogger<ProductController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController(ILogger<ProductController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
+        public IDbConnection Connection
+        {
+            get
+            {
+                //System.Configuration.ConfigurationManager.AppSettings["ConnectionString"]
+                return new SqlConnection(_configuration.GetConnectionString("DatabaseConnection"));
+            }
+        }
+
+        [HttpGet("")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet("DapperTest")]
+        public IActionResult DapperTest()
+        {
+            using (IDbConnection dbconnection = Connection)
+            {
+                string sQuery = @"select * from Products";
+                dbconnection.Open();
+                var list = dbconnection.Query<Products>(sQuery).ToList();
+                return View(list);
+            }
+
+        }
     }
 }
---
+++ dotnet-project/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnet_project.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Quantity { get; set; }
+        public string Price { get; set; }
+        public int SellerId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime SoldAt { get; set; }
+        public int ProductTypeId { get; set; }
+
+    }
+}
