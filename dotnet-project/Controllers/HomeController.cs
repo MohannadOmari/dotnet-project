3af43e0440ba70c2a8d672efd5dec3e9cb5b7f02
Mohannad <mohannad.alumari@gmail.com>
2022-10-23 14:35:54 +0300

    added login and register pages

--- dotnet-project/Controllers/HomeController.cs
+++ dotnet-project/Controllers/HomeController.cs
@@ -18,6 +18,18 @@ public class HomeController : Controller
         return View();
     }
 
+    [HttpGet("Login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpGet("Register")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
