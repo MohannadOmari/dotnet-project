fa8f5b2fc79e2411fee0b3014e50a991e1313d06
Mohannad <mohannad.alumari@gmail.com>
2022-10-25 10:47:38 +0300

    added usercontroller

--- dotnet-project/Controllers/HomeController.cs
+++ dotnet-project/Controllers/HomeController.cs
@@ -18,32 +18,6 @@ public class HomeController : Controller
         return View();
     }
 
-    [HttpGet("Login")]
-    public IActionResult Login()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Login(Users user) //add functionality
-    {
-        return Redirect("Index");
-    }
-
-    [HttpGet("Register")]
-    public IActionResult Register()
-    {
-        return View();
-    }
-
-    [HttpPost("Register")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Register(Users user) //add functionality
-    {
-        return RedirectToAction("Login");
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
